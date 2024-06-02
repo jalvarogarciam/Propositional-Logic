5ff5493454f7b35d7056b75a945e61c31efc30f5
jalvarogarciam <josealvarogarmarquez@gmail.com>
2024-06-02 20:17:46 +0200

    punto de partida

---
+++ LogicExpression.py
@@ -0,0 +1,416 @@
+from conectors import usual_to_polish, unary_connectors, binary_connectors, connectors, DEBUG, \
+dec_binbol, all_connectors
+
+
+
+
+
+class LogicExpression:
+    '''
+    The shape of LogicExpression's are like a "tree of expression". But, the main
+    type is a Unary or Binary Connective. 
+
+    ex:CONNECTIVE(CONNECTIVE(CONNECTIVE(...),CONNECTIVE(...))) on his leafs, we can
+    found bool vars x,y,z...
+
+    And then, it will be able to evaluate the expression with a __call__.
+    '''
+    ##########################################################################
+    def __init__(self, arg:str, root:"LogicExpression" = None, counter=0):
+        self.counter = counter + 1     #strange things about recursibity
+
+        self.root = root
+
+        self.notation = {'!':'!','&':'&','|':'|','>':'>','=':'=','<':'<'}
+
+        self.__argument : LogicExpression|str|bool|int  #and subclasses
+        self.vars: list = []
+        self.type: str
+
+        self.__clausular_set = set("empty")
+
+
+
+        if type(arg) == str: 
+
+            if len(arg) > 1 : self.expr__init__(arg)
+            elif arg.isdigit() : self.bool__init__(arg)
+            else: self.proposition__init__(arg)
+
+
+        elif type(arg) in (bool,int): self.bool__init__(arg)
+
+        #type(arg) in (AND,OR,IMPLY,BICOND):
+        elif type(arg) in (tuple,list) : self.connective__init__(arg)
+
+        elif type(arg) == LogicExpression : self.copy__init__(arg)
+
+
+    ##########################################################################
+    #Expression builder
+    def expr__init__(self, raw_expression:str|list|tuple):
+
+        if self.counter == 1:
+
+            raw_expression, self.notation = usual_to_polish(raw_expression)
+
+        self.type = raw_expression[0]
+
+        self.find_vars(raw_expression)
+
+        #BUILDS A NEW L.E. FOR ARGUMENT######################################
+        
+        #UNARY CONNECTIVES (!)
+        if self.type in unary_connectors:
+
+            self.__argument = LogicExpression(raw_expression[1:], self, self.counter)
+
+        #BINARY CONNECTIVES (|,&,<,>,=)
+        else:   # self.type in binary_connectors:
+
+            #LOCATES the range of the main connective
+            #It's the same algorithm that we use to check if a polish expr is correct
+            in_range,i = 1,1
+            while in_range != 0 :
+                if raw_expression[i] in binary_connectors: 
+                    in_range+=1
+                    
+                elif raw_expression[i] not in unary_connectors: in_range-=1
+                i+=1
+
+            #ARGUMENT BUILDINT
+            self.__argument = [LogicExpression(raw_expression[1:i], self, self.counter), \
+                                LogicExpression(raw_expression[i:], self, self.counter)]
+
+            #ASOCIATIVE PROPERTY
+            if self.type in ('|','&'): self.asociate()
+
+            #REDUCES EACH < TO A >
+            elif self.type == '<':
+                self.type = '>'
+                self[0], self[1] = self[1], self[0]
+
+            #simplify 0's and 1's
+            self.check_neutral_and_dominant()
+
+    ##########################################################################
+    #Proposition builder
+    def proposition__init__(self, proposition:str|list|tuple):
+
+        self.__argument = str(proposition[0])    #always is a char
+        self.vars = [proposition[0]]
+        self.type = 'p'
+
+    ##########################################################################
+    #Bool builder
+    def bool__init__(self, true:bool|int):
+
+        self.__argument = bool(int(true))
+        self.vars.append(str(int(true)))
+        self.type = '1' if bool(int(true)) else '0'
+
+    ##########################################################################
+    #Copy Builder
+    def copy__init__(self, other: 'LogicExpression'):
+        self.counter = 1
+
+        self.type = other.type
+
+        self.notation = other.notation
+
+        self.vars = other.vars
+
+        if other.type in binary_connectors:
+            self.__argument = other.__argument.copy()
+        elif other.type in unary_connectors:
+            self.__argument = LogicExpression(other.__argument, self, self.counter)
+        else:
+            self.__argument = other.__argument
+
+
+    ##########################################################################
+    #connective Builder (list)
+    def connective__init__(self,le_list:list|tuple):
+        self.type = le_list[0]
+        self.__argument = le_list[1:]
+
+        if self.type == '!': 
+            self.vars = self.__argument.vars
+        
+        elif self.type in binary_connectors:
+            for i in range(len(self.__argument)):
+                self.vars = set(self.vars) | set(self[i].vars)
+            self.vars = list(self.vars)
+            self.vars = sorted(self.vars)
+
+    ##########################################################################
+    ##########################################################################
+
+
+    def __call__(self,vars={})->bool:
+        truth : bool = False
+        
+        if vars == {}: return self.truth_table()
+
+        if self.type == 'p': truth = bool(vars[self.__argument])
+
+        elif self.type in ('0','1'): truth = bool(self.__argument)
+
+        elif self.type == '!': truth = not bool(self.__argument(vars))
+
+        elif self.type in binary_connectors:
+
+            truth_list:list = self.__argument.copy()
+
+            #TURNS each argument INTO A BOOL VALUE
+            #operator __call__ -> evaluate
+            for i in range(len(truth_list)): truth_list[i] = truth_list[i](vars)
+
+            if self.type == '&':
+                truth = True
+                for i in range(len(truth_list)): truth &= truth_list[i]
+
+            elif self.type == '|':
+                truth = False
+                for i in range(len(truth_list)): truth |= truth_list[i]
+
+            elif self.type == '>':
+                truth = (not truth_list[0]) or truth_list[1]
+
+            elif self.type == '=':
+                truth = ((not truth_list[0]) or truth_list[1]) and \
+                ((not truth_list[1]) or truth_list[0])
+
+        return (1 if truth else 0)
+
+    #
+    def find_vars(self,string:str=" "):
+
+        if string == " ": string = str(self)
+        self.vars = []
+        for i in string:
+            if (i.isalpha() or i.isdigit()) and not (i in self.vars): self.vars.append(i)
+        
+        if len(self)>1: self.vars = sorted(self.vars)
+
+
+    def truth_table(self)->str:
+
+        string = ('.'*20 + '\n')*2
+        self.find_vars()
+
+        values = dict({})
+        for v in self.vars: values.update({v:0})
+
+        num_vars = len(self.vars)
+
+        if self.type in ('1','0'):
+            string += self.type + '---> ' +self.type + '\n'
+
+        else:
+            for v in values: string += v
+            string +='\n'
+
+            for bit in range(2**num_vars):
+                bits = dec_binbol(bit,num_vars)
+                i = 0
+                for k in values.keys():
+                    values[k]=bits[i]
+                    string += str(int(bits[i]))
+                    i+=1
+                string += '---> ' + str(bool(self(values))) + '\n'
+
+        string += ('.'*20 +'\n')*2
+
+        return string
+    
+
+    def to_canonical_shape(self,on_minterms=True)->'LogicExpression':
+        self.find_vars()
+        num_vars = len(self.vars)
+        minterms = tuple(self.minterms())
+        maxterms = tuple(set(range(2**num_vars)) - set(minterms))
+        
+        terms = minterms if on_minterms else maxterms
+
+        symbol = '&' if on_minterms else '|'
+
+        le_list = []
+        for i in terms:
+            binary = dec_binbol(i,num_vars)
+
+            string = ""
+            i=0
+            for bit in binary:
+                
+                oposite = '!' if bit else ''
+                string += oposite + str(self.vars[i]) + symbol
+                i += 1
+            
+            string = string[:-1]
+            
+            string = '('+string+')'
+
+            le_list.append(LogicExpression(string, self, self.counter))
+
+        symbol = '&' if symbol=='|' else '|'
+        le_list.insert(0,symbol)
+
+        copy = LogicExpression(le_list) if len(le_list) > 1 else LogicExpression(str(self))
+
+        return copy
+
+
+    def minterms(self)->set: return self.canonical_terms(True)
+    def maxterms(self)->set: return self.canonical_terms(False)
+    def canonical_terms(self,on_minterms=True):
+        self.find_vars()
+
+        terms = []
+
+        values = dict({})
+        for v in self.vars: values.update({v:0})
+
+        num_vars = len(self.vars)
+
+        for bit in range(2**num_vars):
+
+            bits = dec_binbol(bit,num_vars)
+
+            i = 0
+            for k in values.keys():
+                values[k]=bits[i]
+                i+=1
+
+            if on_minterms:
+                if self(values): terms.append(bit)
+            else: 
+                if not self(values): terms.append(bit)
+        
+        return set(terms)
+
+
+
+    def check_neutral_and_dominant(self):
+        i=0
+        while i < (len(self)):
+
+            if self[i].type == ('0'):
+
+                if self.type == '|': 
+                    del self[i]
+                    if len(self) == 1: self.copy__init__(self.__argument[0])
+
+                elif self.type == '&':
+                    self.bool__init__(0)
+                
+                elif self.type == '>' and i==0:
+                    self.bool__init__(1)
+
+            elif self[i].type == ('1'):
+
+                if self.type == '&': 
+                    del self[i]
+                    if len(self) == 1: self.copy__init__(self.__argument[0])
+
+                elif self.type == '|':
+                    self.bool__init__(1)
+            i+=1
+
+
+
+    def asociate(self):
+        if type(self.type) not in ('|','&'): return None
+
+        term_index = 0
+
+        for x in range(len(self)):
+
+            if self[term_index].type == self.type:
+
+                lenght = len(self[term_index])
+
+                i = lenght - 1
+                while i >= 0:
+                    self.__argument.insert(term_index+1, self[term_index][i])
+                    i-=1
+                del self[term_index]
+                term_index += lenght
+        
+            else: term_index += 1
+
+
+    def __getitem__(self,index): 
+        if len(self) == 1 : return LogicExpression(self.__argument)
+        else : return LogicExpression(self.__argument[index])
+
+    def __setitem__(self, index, value):
+        if len(self) == 1 : self.__argument = LogicExpression(value)
+        else : self.__argument[index] = LogicExpression(value)
+    
+    def __delitem__(self,index):
+        if len(self) == 1 : del self.__argument
+        else : del self.__argument[index]
+
+    def __len__(self)->int:
+
+        if self.type in binary_connectors: 
+            return len(self.__argument) #it's a list
+        
+        else: return 1
+
+    def __str__(self)->str:
+
+        string = ""
+
+
+        if self.type in binary_connectors:
+        
+            for i in range(len(self)):
+
+                string += str(self[i]) + ' ' + self.type + ' '
+
+            string = '(' + string[:-3] + ')' # adds () and removes the last connective
+            
+
+        if self.type in unary_connectors:
+        
+            if self.__argument.type == 'p':
+                string += self.type + str(self.__argument)
+        
+            elif self.__argument.type in ('0','1'): 
+                string+= str(int(not bool(self[0][0])))
+
+            elif len(self.__argument) > 1:
+                string += self.type + '(' + str(self.__argument) + ')'
+        
+        elif self.type in ('p','0','1'):
+            string += self.__argument if self.type == 'p' else \
+                        str(int(self.__argument))
+
+        '''
+        #TURNS CONNECTIVES IN ITS INITIAL NOTATION
+        if self.counter == 1 and type(self.__argument) not in (str, bool, int):
+            string = list(string)
+
+            for i in range(len(string)):
+
+                if string[i] in self.notation.keys():
+                    string[i] = self.notation[string[i]]
+
+            string = string[1:-1]   #DELETE PARENTHESES (...)
+
+            string = "".join(string)
+        '''
+        return string
+
+
+
+cadena = '((a → b) → c) → (a → (b → c))'
+
+expr = LogicExpression(cadena)
+
+
+print(expr())
+
+
---
+++ conectors.py
@@ -0,0 +1,174 @@
+DEBUG = [False,False,True,False,False]
+
+#Conectivas binarias
+binary_connectors= ['&','|','>','=','<']
+all_binary_connectors= {
+    '∨':'|', '+':'|', '||':'|', '|':'|',
+    '∧':'&', '&&':'&', '&':'&', '*':'&',
+    '→':'>','=>':'>','>':'>', '<':'<',
+    '↔':'=','=':'='
+}
+
+#Conectivas unarias
+unary_connectors=['!']
+all_unary_connectors= {
+    '¬':'!', '~':'!', '!':'!'
+}
+
+connectors = list(binary_connectors) + list (unary_connectors)
+all_connectors = list(all_binary_connectors.keys()) + list (all_unary_connectors.keys())
+
+
+def usual_to_polish(arg:list|tuple|str, counter=0)->str:
+    counter += 1
+
+
+    arg = list(arg)
+
+
+
+    if counter == 1:
+
+        #Removes spaces
+        i = 0
+        while i < len(arg):
+            if arg[i].isspace(): del arg[i]
+            else : i+=1
+
+
+        #Unify the notation and save it
+        its_notation = {'!':'!','&':'&','|':'|','>':'>','=':'=','<':'<'}
+        i=0
+        while i < len(arg)-1:
+
+            #unary connectors
+            if arg[i] in all_unary_connectors.keys():
+                its_notation[all_unary_connectors[arg[i]]] = arg[i]
+                arg[i] = all_unary_connectors[arg[i]]
+
+            #double binary connectors like &&
+            elif arg[i] + arg[i+1] in all_binary_connectors.keys():
+                its_notation[all_binary_connectors[arg[i] + arg[i+1]]] = arg[i] + arg[i+1]
+                arg[i:i+2] = all_binary_connectors[arg[i] + arg[i+1]]
+
+            #binary connectors like
+            elif arg[i] in all_binary_connectors.keys():
+                its_notation[all_binary_connectors[arg[i]]] = arg[i]
+                arg[i] = all_binary_connectors[arg[i]]
+
+            #puts missing connectors
+            elif arg[i] not in ('(',')') and arg[i+1] not in ('(',')') and\
+                arg[i+1] not in all_connectors :
+                arg.insert(i+1,'&')
+                i+=1
+
+            i+=1
+
+
+        #if it's on polish notation, return
+        i = 0
+        while arg[i] in all_unary_connectors.keys(): i+=1
+        if arg[i] in all_binary_connectors: return "".join(arg) 
+
+
+        #UNIFIES UNARY CONNECTIVES WITH PROPOSITIONS
+        i = 0
+        while i < len(arg):
+
+            if arg[i] in unary_connectors and  arg[i+1] != '(':
+                arg[i:i+2] = [arg[i]+arg[i+1]]
+            i+=1
+
+
+        #REMOVES ALL NOT(NOT(...))
+        i=0
+        while i+1 < len(arg):
+            if (arg[i] == arg[i+1]) and arg[i] == '!':
+                for x in range(2) : del arg[i]
+            i+=1
+
+
+
+
+    opposite = arg[0] if arg[0] in unary_connectors else ""
+
+    expr = list(arg) if not opposite else list(arg[1:])
+
+
+    #SOLVE ALL PARENTHESES
+    if '(' in expr:
+
+        i = 0
+        while i < len(expr):
+
+            if expr[i] == '(':
+
+                #localizes the main expr between the parenthesses
+                j = i
+                open_par = 1
+                while open_par > 0:
+                    j+=1
+                    if expr[j] == '(': open_par+=1
+                    elif expr[j] == ')': open_par-=1
+
+                #turns parenthesses into polish notation
+                expr[i:j+1] = [usual_to_polish(expr[i+1:j], counter)]
+
+            else: i += 1
+
+    #FOR BINARY CONNECTIVES --> FORM P CONNTV P TO CONNTV P P
+    i = 0
+    while len(expr) > 1 :
+        if (expr[i] in binary_connectors):   #(prop) Cnective (prop)
+            expr[i-1:i+2] = [expr[i] + expr[i-1] + expr[i+1]]   #Cnective (prop) (prop)
+
+        else: i+=1
+
+
+    if len(opposite) > 0: expr.insert(0,opposite)
+
+    if counter == 1:
+        return ("".join(expr),its_notation)
+    else:
+        return "".join(expr)
+
+
+
+
+def test (expresion:str)-> int:
+    counter=1
+    for char in expresion:
+        if char in binary_connectors:
+            counter+=1
+        elif char not in unary_connectors:
+            counter-=1
+    
+    return counter
+
+
+
+from math import log2
+
+
+
+def dec_binbol(int_number:int,bits:int=None)->tuple:
+        #adjust the default parameter
+        if bits!=None and int_number>=2**bits:
+            return None
+        elif bits==None:
+            bits=int(log2(int_number))+1
+
+        binbol=[]
+
+        #from decimal to binary
+        for i in range(bits):
+            binbol.insert(0,int_number%2)
+            int_number//=2
+
+        #From binary to bolean
+        '''for i in range(len(binbol)):
+            if binbol[i]==1 :
+                binbol[i]=True
+            else :
+                binbol[i]=False'''
+        return tuple(binbol)
